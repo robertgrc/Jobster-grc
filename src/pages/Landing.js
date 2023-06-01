3c0bbc5f4695706721baeb9b93181e6934a49234
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-06-01 15:19:53 -0400

    modulo 20 finishLandingPage

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,11 @@
 import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
 import React from 'react'
+import styled from 'styled-components';
 
 const Landing = () => {
   return (
-    <main>
+    <Wrapper>
         <nav>
             <img src={logo} alt='jobster logo' className='logo'/>
         </nav>
@@ -19,8 +20,47 @@ const Landing = () => {
           </div>
           <img src={main} alt='jon hunt' className='img main-img'/>
         </div>
-    </main>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.main`
+    nav{
+        width: var(--fluid-width);
+        max-width: var(--max-width);
+        margin: 0 auto;
+        height: var(--nav-height)
+        display: flex;
+        align-items: center;
+    }
+
+    .page{
+        min-height: calc(100vh - var(--nav-height));
+        display: grid;
+        align-items: center;
+        margin-top: -3 rem;
+    }
+    h1{
+        font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+    }
+    p {
+        color: var(--grey-600)
+    }
+    .main-img {
+       display: none; 
+    }
+    @media (min-width:992px){
+      .page{
+        grid-template-columns: 1fr 1fr;
+        column-gap: 3rem;
+      }
+      .main-img {
+        display: block;
+      }
+    }
+`;
 
 export default Landing;
