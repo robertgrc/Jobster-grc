6aa3dd6d059ba1af7d1ca108f151c63b9865d401
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-06-06 23:59:05 -0400

    578. Register and Login Placeholders

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import { Logo, FormRow } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
 import { toast } from 'react-toastify';
+import { useSelector, useDispatch } from 'react-redux'
+import { loginUser, registerUser } from '../features/user/userSlice';
 
 const initialState = {
   name: '',
@@ -13,6 +15,9 @@ const initialState = {
 const Register = () => {
 
   const [values, setValues] = useState(initialState)
+  
+  const { user, isLoading } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     const name = e.target.name
     const value = e.target.value
@@ -27,7 +32,13 @@ const Register = () => {
     const {name, email, password, isMember} = values
     if(!email || !password || (!isMember && !name)){
       toast.error('please fill out all fields');
+      return;
     }
+    if(isMember){
+      dispatch(loginUser({ email: email, password: password}))
+      return;
+    }
+    dispatch(registerUser({name, email, password}))
   }
 
   const toggleMember = () => {
